30a4347eca78d0c7ae108e9dd18295817194c07d
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-06-28 10:19:47 -0500

    First API testing Rev3

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -29,29 +29,24 @@ describe('First Api Tests', () => {
     const response = await axios.head('https://httpbin.org/get');
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.headers).to.not.equal(null);
-    console.log("Status is ",response.status);
   });
 
   it('Consume POST Service', async () => {
     const response = await axios.post('https://httpbin.org/post',newuser);
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.data.json).to.have.property("name");
-    console.log("Status is ",response.status);
-    console.log("Name of new user is ",response.data.json.name, " , age is ", response.data.json.age, " and activity is ", response.data.json.activity);
   });
 
   it('Consume PATCH Service', async () => {
     const response = await axios.patch('https://httpbin.org/patch',newuser);
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.data.json).to.have.property("age");
-    console.log("Status is ",response.status);
   });
 
   it('Consume PUT Service', async () => {
     const response = await axios.put('https://httpbin.org/put',newuser);
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.data.json).to.have.property("born_in");
-    console.log("Status is ",response.status);
   });
   
   it('Consume DELETE Service', async () => {
