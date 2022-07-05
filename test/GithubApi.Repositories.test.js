bcbc51cdbb0810a5fe7467efdec8b413f75ef79e
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-07-04 21:53:58 -0500

    Trying to fix before all hook for error

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -10,44 +10,49 @@ chai.use(chaiSubset);
 const urlrepos = 'https://api.github.com/repos/aperdomob/jasmine-json-report';
 
 describe('Repositories GET', () => {
+  let userResponse;
+
+  before(async () => {
+    userResponse = await axios.get('https://api.github.com/users/aperdomob');
+  });
   it('Consume GET Service on GITHUB API', async () => {
-    const response = await axios.get('https://api.github.com/users/aperdomob');
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data.name).to.equal('Alejandro Perdomo');
-    expect(response.data.company).to.equal('Perficient Latam');
-    expect(response.data.location).to.equal('Colombia');
+    expect(userResponse.status).to.equal(StatusCodes.OK);
+    expect(userResponse.data.name).to.equal('Alejandro Perdomo');
+    expect(userResponse.data.company).to.equal('Perficient Latam');
+    expect(userResponse.data.location).to.equal('Colombia');
   });
 
-  it('Finding by Hypermedia', async () => {
-    const response = await axios.get('https://api.github.com/users/aperdomob/repos');
-    const repo = response.data;
-    function isjasmine(nombre) {
-      return nombre.name === 'jasmine-json-report';
-    }
-    const jasmine = repo.find(isjasmine);
-    expect(jasmine.name).to.equal('jasmine-json-report');
-    expect(jasmine.private).to.equal(false);
-    expect(jasmine.description).not.equal(null);
-  });
-  it('Downloading by zip', async () => {
-    const response = await axios.get(`${urlrepos}/zipball/master`);
-    expect(response.status).to.equal(StatusCodes.OK);
-  });
-  it('Finding REAMDE', async () => {
-    const response = await axios.get('https://api.github.com/repos/aperdomob/jasmine-json-report/contents');
-    const obj = response.data;
-    expect(obj).to.containSubset([{
-      name: 'README.md',
-      path: 'README.md',
-      sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
-    }]);
-    expect(response.status).to.equal(StatusCodes.OK);
-  });
-  it('Downloading REAMDE and checking md5', async () => {
-    const response = await axios.get('https://raw.githubusercontent.com/aperdomob/jasmine-json-report/master/README.md');
-    const obj = response.data;
-    const hash = md5(obj);
-    console.log(`MD5 is ${hash}`);
-    expect(hash).not.equal(null);
+  describe('Using hypermedia', () => {
+    let jasmineRepo;
+    before(async () => {
+      const repositoriesResponse = await axios.get(`${userResponse.repos_url}`);
+      jasmineRepo = repositoriesResponse.data.find((repo) => repo.name === 'jasmine-json-report');
+    });
+    it('Finding by Hypermedia', async () => {
+      expect(jasmineRepo.name).to.equal('jasmine-json-report');
+      expect(jasmineRepo.private).to.equal(false);
+      expect(jasmineRepo.description).not.equal(null);
+    });
+    it('Downloading by zip', async () => {
+      const response = await axios.get(`${urlrepos}/zipball/master`);
+      expect(response.status).to.equal(StatusCodes.OK);
+    });
+    it('Finding REAMDE', async () => {
+      const response = await axios.get('https://api.github.com/repos/aperdomob/jasmine-json-report/contents');
+      const obj = response.data;
+      expect(obj).to.containSubset([{
+        name: 'README.md',
+        path: 'README.md',
+        sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
+      }]);
+      expect(response.status).to.equal(StatusCodes.OK);
+    });
+    it('Downloading REAMDE and checking md5', async () => {
+      const response = await axios.get('https://raw.githubusercontent.com/aperdomob/jasmine-json-report/master/README.md');
+      const obj = response.data;
+      const hash = md5(obj);
+      console.log(`MD5 is ${hash}`);
+      expect(hash).not.equal(null);
+    });
   });
 });
