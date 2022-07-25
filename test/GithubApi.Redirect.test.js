66c32e350cccbd0bf50712b850147e6a4b42096f
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-07-24 22:40:08 -0500

    Head method and query parameters Rev1

--- test/GithubApi.Redirect.test.js
+++ test/GithubApi.Redirect.test.js
@@ -6,23 +6,24 @@ const chaiSubset = require('chai-subset');
 
 chai.use(chaiSubset);
 
+const userUrl = 'https://github.com/aperdomob/redirect-test';
+const userReUrl = 'https://github.com/aperdomob/new-redirect-test';
+
 describe('Repositories HEAD', () => {
   let userResponse;
   let userResponse2;
   before(async () => {
-    userResponse = await axios.head('https://github.com/aperdomob/redirect-test');
-    userResponse2 = await axios.head('https://github.com/aperdomob/new-redirect-test');
+    userResponse = await axios.head(`${userUrl}`);
+    userResponse2 = await axios.head(`${userReUrl}`);
   });
   it('Consume HEAD Service with redirect', () => {
     expect(userResponse.status).to.equal(StatusCodes.OK);
     expect(userResponse2.status).to.equal(StatusCodes.OK);
-    // console.log(userResponse.status);
-    console.log(userResponse.headers.Location);
-    console.log(userResponse2.headers);
+    expect(userResponse.request.res.responseUrl).to.equal(`${userReUrl}`);
   });
   it('Consume GET Service with redirect', async () => {
-    const response = await axios.get('https://github.com/aperdomob/redirect-test');
+    const response = await axios.get(`${userUrl}`);
     expect(response.status).to.equal(StatusCodes.OK);
-    console.log(response.body);
+    expect(response.request.res.responseUrl).to.equal(`${userReUrl}`);
   });
 });
